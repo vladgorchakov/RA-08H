5a62fb13afc70dfd5aa3d698198a83b40240254e
vladislav <vladislavzikstudy@gmail.com>
2023-02-21 18:22:31 +0300

    add parser

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ class BaudrateException(Exception):
     def __str__(self):
         return f"{self.baudrate} is invalid. Baudrate should not be more than 9600"
 
+
 class RA08H:
     def __init__(self, port, baudrate):
         self.uart = serial.Serial(port=port, baudrate=baudrate)
-
 
     def connect(self):
         self.uart.open()
@@ -33,45 +33,32 @@ class RA08H:
         sleep(timeout)
         return self.uart.read_all().decode("utf-8").split("\r\n")
 
+    def parse(self, request):
+        if request[2] == 'OK':
+            if ':' in request[1]:
+                return request[1].split(':')[1]
+            elif '=' in request[1]:
+                return request[1].split('=')[1]
+
     def read_manufacturer_identification(self) -> dict:
         at_command = b"AT+CGMI?\r\n"
-        request = self.send_command(at_command, 1)
-        print(request)
-
-        if request[2] == 'OK':
-            return {'status': request[2], 'manufacturer': request[1][6:]}
-        else:
-            return {'status': 'error'}
+        return self.parse(self.send_command(at_command, 0.1))
 
     def read_model_identification(self):
         at_command = b"AT+CGMM?\r\n"
-        request = self.send_command(at_command, 0.5)
-        if request[2] == 'OK':
-            return {'status': request[2], 'model': request[1][6:]}
-        else:
-            return {'status': 'error'}
+        return self.parse(self.send_command(at_command, 0.1))
 
     def read_version_identification(self):
         at_command = b"AT+CGMR?\r\n"
-        request = self.send_command(at_command, 0.5)
-        if request[2] == 'OK':
-            return {'status': request[2], 'version': request[1][6:]}
-        else:
-            return {'status': 'error'}
+        return self.parse(self.send_command(at_command, 0.1))
 
     def read_product_sequence_number(self):
         at_command = b"AT+CGSN?\r\n"
-        request = self.send_command(at_command, 0.5)
-        if request[2] == 'OK':
-            return {'status': request[2], 'sn': request[1][6:]}
-        else:
-            return {'status': 'error'}
+        return self.parse(self.send_command(at_command, 0.1)[1:])
 
     def read_baudrate(self):
         at_command = b"AT+CGBR?\r\n"
-        request = self.send_command(at_command, 0.5)
-        if request[2] == 'OK':
-            return request[1][6:]
+        return self.parse(self.send_command(at_command, 0.1))
 
     def set_baudrate(self, baudrate):
         if baudrate > 9600:
@@ -81,15 +68,14 @@ class RA08H:
 
     def read_join_mode(self):
         at_command = b"AT+CJOINMODE?\r\n"
-        request = self.send_command(at_command, 0.5)
-        if request[2] == 'OK':
-            if request[1] == '+CJOINMODE:0':
-                return 'OTAA'
-            elif request[1] == '+CJOINMODE:1':
-                return 'ABR'
+        mode = self.parse(self.send_command(at_command, 0.1))
+        if mode == '0':
+            return 'OTAA'
+        else:
+            return 'ABR'
 
     def set_join_mode(self, mode):
-        if mode == 'OTTA':
+        if mode == 'OTAA':
             at_command = b"AT+CJOINMODE=0\r\n"
         elif mode == 'ABR':
             at_command = b"AT+CJOINMODE=1\r\n"
@@ -100,6 +86,29 @@ class RA08H:
         if request[1] == 'OK':
             return True
 
+    def read_dev_eui(self):
+        at_command = b"AT+CDEVEUI?\r\n"
+        return self.parse(self.send_command(at_command, 0.5))
+
+    def set_dev_eui(self, devEUI):
+        at_command = bytes(f"AT+CDEVEUI={devEUI}\r\n".encode())
+        request = self.send_command(at_command, 0.5)
+
+        if request[1] == "OK":
+            return True
+
+    def read_app_eui(self):
+        at_command = b"AT+CAPPEUI?\r\n"
+        return self.parse(self.send_command(at_command, 0.5))
+
+    def set_app_eui(self, app_eui):
+        at_command = bytes(f"AT+CAPPEUI={app_eui}\r\n".encode())
+        request = self.send_command(at_command, 0.5)
+
+        if request[1] == 'OK':
+            return True
+
+
 if __name__ == "__main__":
     ra = RA08H('COM7', 9600)
     print(ra.read_manufacturer_identification())
@@ -107,6 +116,8 @@ if __name__ == "__main__":
     print(ra.read_version_identification())
     print(ra.read_product_sequence_number())
     print(ra.read_join_mode())
-    print(ra.set_join_mode('OTTA'))
-    print(ra.read_baudrate())
-    print(ra.read_manufacturer_identification())
+    print(ra.set_join_mode('ABR'))
+    print(ra.set_dev_eui("1000000030000005"))
+    print(ra.read_dev_eui())
+    print(ra.set_app_eui("0000000000000099"))
+    print(ra.read_app_eui())
